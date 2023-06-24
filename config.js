d255a186748343d94ba43314d0df5b5eed2b4bd7
gitfev <137526071+gitfev@users.noreply.github.com>
2023-06-24 09:40:14 -0400

    yay

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   config: {
     "domain": "https://discord.lendmark.sk",
     "id": "865892082354618368",
-    "secret": "secret",
-    "bottoken": "token"
+    "secret": "L912UIstB1Ihg5ZzzGziEHfxv0GTlu_P",
+    "bottoken": "MTEyMjE0MzY4MTUxMjc1MTIyNA.GtWiYZ.4L5LlOsBcAk2wlxCPs0oakUh34PR-GBQrC0ayY"
   }
 }
